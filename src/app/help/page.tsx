5819fdc40b0c2cf91c7323314b24072f23d646d4
spherele <maks.tolkachev.99@gmail.com>
2024-07-11 00:26:11 +0700

    FIX: page Help

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
+import classNames from 'classnames';
 import DeliveryInformation from '@/components/ui/DeliveryInformation/DeliveryInformation';
 import classes from './HelpSection.module.css';
-import BackwardLink from "@/components/ui/BackwardLink/BackwardLink";
+import styles from './HelpSection.module.css';
+import BackwardLink from '@/components/ui/BackwardLink/BackwardLink';
+import Link from 'next/link';
 
-const HelpSection : React.FC = () => {
+const HelpSection: React.FC = () => {
     return (
-        <main className='container'>
-            <div className={classes.HelpSection}>
-                <div className={classes.container}>
-                    <BackwardLink href='/' text='На главную' />
-                    <h1>Помощь</h1>
-                    <ul>
-                        <li><a href="/contacts">Контакты</a></li>
-                        <li><a href="/#size-chart">Размерная таблица</a></li>
-                        <li><a href="/#payment">Оплата товара</a></li>
-                        <li><a href="/#delivery-terms">Условия доставки</a></li>
-                        <li><a href="/#returns">Возврат товара</a></li>
-                        <li><a href="/#care-symbols">Знаки и уход за товарами</a></li>
-                        <li><a href="/#promotions">Акции</a></li>
-                    </ul>
-                </div>
-                <DeliveryInformation/>
+        <main className={classNames(classes.container, 'container', classes.HelpSection)}>
+            <BackwardLink href='/' text='На главную' className={classes.backwardLinkMargin}/>
+            <h1 className={classNames(classes.title)}>Помощь</h1>
+            <div className={classNames(classes.HelpSection__links)}>
+                <Link href="/contacts" className={classNames(classes.HelpSection__link)}>Контакты</Link>
+                <Link href="/#size-chart" className={classNames(classes.HelpSection__link)}>Размерная таблица</Link>
+                <Link href="/#payment" className={classNames(classes.HelpSection__link)}>Оплата товара</Link>
+                <Link href="/#delivery-terms" className={classNames(classes.HelpSection__link)}>Условия доставки</Link>
+                <Link href="/#returns" className={classNames(classes.HelpSection__link)}>Возврат товара</Link>
+                <Link href="/#care-symbols" className={classNames(classes.HelpSection__link)}>Знаки и уход за товарами</Link>
+                <Link href="/#promotions" className={classNames(classes.HelpSection__link)}>Акции</Link>
             </div>
+            <DeliveryInformation/>
         </main>
     );
 };
 
 export default HelpSection;
+
