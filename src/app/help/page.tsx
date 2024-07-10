9b6b9e7e6372a22318b3ea8c0de1788e37b8be13
spherele <maks.tolkachev.99@gmail.com>
2024-07-10 15:59:18 +0700

    FEAT: add page Help (site.url/help)

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import DeliveryInformation from '@/components/ui/DeliveryInformation/DeliveryInformation';
 import classes from './HelpSection.module.css';
+import BackwardLink from "@/components/ui/BackwardLink/BackwardLink";
 
 const HelpSection : React.FC = () => {
     return (
-        <div className={classes.HelpSection}>
-            <div className={classes.container}>
-                <h1>Помощь</h1>
-                <ul>
-                    <li><a href="/contacts">Контакты</a></li>
-                    <li><a href="/size-chart">Размерная таблица</a></li>
-                    <li><a href="/payment">Оплата товара</a></li>
-                    <li><a href="/delivery-terms">Условия доставки</a></li>
-                    <li><a href="/returns">Возврат товара</a></li>
-                    <li><a href="/care-symbols">Знаки и уход за товарами</a></li>
-                    <li><a href="/promotions">Акции</a></li>
-                </ul>
+        <main className='container'>
+            <div className={classes.HelpSection}>
+                <div className={classes.container}>
+                    <BackwardLink href='/' text='На главную' />
+                    <h1>Помощь</h1>
+                    <ul>
+                        <li><a href="/contacts">Контакты</a></li>
+                        <li><a href="/#size-chart">Размерная таблица</a></li>
+                        <li><a href="/#payment">Оплата товара</a></li>
+                        <li><a href="/#delivery-terms">Условия доставки</a></li>
+                        <li><a href="/#returns">Возврат товара</a></li>
+                        <li><a href="/#care-symbols">Знаки и уход за товарами</a></li>
+                        <li><a href="/#promotions">Акции</a></li>
+                    </ul>
+                </div>
+                <DeliveryInformation/>
             </div>
-            <DeliveryInformation/>
-        </div>
+        </main>
     );
 };
 
