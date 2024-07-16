238dd157b10931abbf46fe646abd45ff6a73b621
spherele <maks.tolkachev.99@gmail.com>
2024-07-16 17:24:32 +0700

    FEAT: add 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,33 @@
 import { FC } from 'react';
+import styles from '@/app/not-found.module.css';
+import Button from '@/components/ui/Button/Button';
+import Link from 'next/link';
+
 
 const NotFound: FC = () => {
-  return (
-    null
-  );
+    return (
+        <main className="container">
+            <h1 className={styles.title}>Страница не найдена</h1>
+            <div className={styles.wrapper}>
+                <div className={styles.picture}>
+                    <img src={'/images/404.png'} alt={'404 Error'}/>
+                </div>
+                <div className={styles.description}>
+                    <h2>Ошибка 404</h2>
+                    <h3>Страница не найдена</h3>
+                    <p>Неправильно набран адрес или такой страницы не существует</p>
+                    <div className={styles.description_btns}>
+                        <Button variant={'negative'} icon={false}>
+                            <Link href={'/'}>Перейти на главную</Link>
+                        </Button>
+                        <Button variant={'default'} icon={false}>
+                            <Link href={'/catalog'}>Вернуться в каталог</Link>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default NotFound;
