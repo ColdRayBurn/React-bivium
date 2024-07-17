948917d769ac2018bf441fbc00d7fb70f5fcb161
spherele <maks.tolkachev.99@gmail.com>
2024-07-17 20:29:04 +0700

    FEAT: add 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,31 +1,38 @@
-import { FC } from 'react';
+import {FC} from 'react';
 import styles from '@/app/not-found.module.css';
-import Button from '@/components/ui/Button/Button';
 import Link from 'next/link';
+import DeliveryInformation from '@/components/ui/DeliveryInformation/DeliveryInformation';
+import classNames from "classnames";
+
 
 
 const NotFound: FC = () => {
     return (
-        <main className="container">
+        <main className={classNames(styles.container, 'container')}>
             <h1 className={styles.title}>Страница не найдена</h1>
-            <div className={styles.wrapper}>
+            <div className={styles.wrapper__not_found}>
                 <div className={styles.picture}>
                     <img src={'/images/404.png'} alt={'404 Error'}/>
                 </div>
                 <div className={styles.description}>
-                    <h2>Ошибка 404</h2>
-                    <h3>Страница не найдена</h3>
-                    <p>Неправильно набран адрес или такой страницы не существует</p>
-                    <div className={styles.description_btns}>
-                        <Button variant={'negative'} icon={false}>
-                            <Link href={'/'}>Перейти на главную</Link>
-                        </Button>
-                        <Button variant={'default'} icon={false}>
-                            <Link href={'/catalog'}>Вернуться в каталог</Link>
-                        </Button>
-                    </div>
+                    Извините, но запрашиваемая Вами страница не <br/>
+                    существует. Возможно, вы ввели неправильный <br/>
+                    адрес или страница была перемещена.<br/><br/>
+
+                    <div style={{fontWeight: 600}}>Что вы можете сделать:</div>
+                    <br/>
+
+                    <ul>
+                        <li>Проверьте правильность введенного адреса.</li>
+                        <li>Перейдите на <Link href={'/'}>главную страницу</Link>.</li>
+                        <li>Используйте поиск на сайте, чтобы найти нужную информацию.</li>
+                        <li><Link href={'/'}>Свяжитесь с нами</Link>, если вы считаете, что это ошибка.</li>
+                    </ul>
                 </div>
             </div>
+            <section className='container'>
+                <DeliveryInformation className={styles.deliveryInformation} withButton/>
+            </section>
         </main>
     );
 };
