43c93f7eb0d95574f14dba1c1da94da8c2eafa80
spherele <maks.tolkachev.99@gmail.com>
2024-07-20 21:34:11 +0700

    FEAT: add News

--- src/app/(home)/NewsSection.tsx
+++ src/app/(home)/NewsSection.tsx
@@ -4,10 +4,12 @@ import { FC, useState } from 'react';
 import { INewsItemList } from '@/api/models';
 import { formatDate } from '@/utils/formatDate';
 import { formatUrl } from '@/utils/formatUrl';
-import { NextArrow, PrevArrow } from '@/components/ui/Arrows/Arrows';
-
 import Link from 'next/link';
-import Slider from 'react-slick';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 import styles from './NewsSection.module.css';
 import classNames from 'classnames';
@@ -19,50 +21,41 @@ interface Props {
 const Page: FC<Props> = ({ initialNews }) => {
     const [news, setNews] = useState<INewsItemList[]>(initialNews);
 
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
-
     return (
         <section>
             <div className={classNames(styles.header, 'container')}>
                 <h1 className={styles.title}>Новости СМИ</h1>
             </div>
-            <Slider {...settings} className={styles.slider}>
+            <Swiper
+                slidesPerView={3}
+                loop={true}
+                autoplay={{ delay: 3000 }}
+                breakpoints={{
+                    2560: {
+                        slidesPerView: 4
+                    },
+                    1024: {
+                        slidesPerView: 3
+                    },
+                    768: {
+                        slidesPerView: 1
+                    },
+                    0: {
+                        slidesPerView: 1
+                    }
+                }}
+                className={styles.slider}
+            >
                 {news.map(newsItem => (
-                    <div key={newsItem.id} className={styles.contentItem}>
+                    <SwiperSlide key={newsItem.id} className={styles.contentItem}>
                         <div>
                             <img src={formatUrl(newsItem.image)} alt={newsItem.name} />
                             <p>{formatDate(newsItem.date)}</p>
                             <Link href={`/news/${newsItem.id}`}><h2>{newsItem.name}</h2></Link>
                         </div>
-                    </div>
+                    </SwiperSlide>
                 ))}
-            </Slider>
+            </Swiper>
         </section>
     );
 };
