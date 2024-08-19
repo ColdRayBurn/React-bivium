a47d3155d11f464262210a7b97263ae8601337cd
spherele <maks.tolkachev.99@gmail.com>
2024-08-19 18:06:51 +0700

    FEAT: Проверка хэша при открытии ссылки на восстановление

--- src/app/(auth)/password-recovery/[id]/page.tsx
+++ src/app/(auth)/password-recovery/[id]/page.tsx
@@ -1,9 +1,41 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useEffect, useState } from 'react';
 import classNames from 'classnames';
 import styles from './page.module.css';
 import Form from './Form';
+import api from '@/api';
+import { useParams } from 'next/navigation';
+import NotFound from '@/app/not-found';
 
 const Page: FC = () => {
+  const [hashValid, setHashValid] = useState(true);
+
+  const params = useParams();
+  const hash = params.id || '';
+
+  useEffect(() => {
+    if (!hash) {
+      setHashValid(false);
+      return;
+    }
+
+    api
+      .post(`authorization/check-hash/${hash}/`)
+      .then(response => {
+        if (response.status !== 200) {
+          setHashValid(false);
+        }
+      })
+      .catch(error => {
+        setHashValid(false);
+      });
+  }, [hash]);
+
+  if (!hashValid) {
+    return <NotFound />;
+  }
+
   return (
     <main className={classNames(styles.wrapper, 'container')}>
       <h1 className={styles.title}>Восстановление пароля</h1>
